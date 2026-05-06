e70417c33d92003f3942a2b2b482ef2d5824ad6e
Gimbol4 <Shishkanov.yd@gmail.com>
2026-05-06 18:22:51 +0300

    solve task06

--- Task06/logic.cpp
+++ Task06/logic.cpp
@@ -8,5 +8,31 @@
 #include "logic.h"
 
 void sort_in_range(int* array, int size, int start, int end, bool type) {
+	if (start < 0) {
+		start = 0;
+	}
+	if (end >= size) {
+		end = size - 1;
+	}
 
+	for (int i = start; i < end; i++)
+	{
+		for (int j = start; j < end - (i - start); j++) {
+			bool condition;
+			if (type) {
+				condition = *(array + j) > *(array + j + 1);
+			}
+			else {
+				condition = *(array + j) < *(array + j + 1);
+			}
+
+			if (condition) {
+				int t = *(array + j);
+				*(array + j) = *(array + j + 1);
+				*(array + j + 1) = t;
+
+			}
+
+		}
+	}
 }
